a94531dd6e8689ec4179fb079fae0a72cb5e0634
MonacoMV <90590179+MonacoMV@users.noreply.github.com>
2022-09-25 16:08:27 -0700

    Update ArchVM.sh

--- ArchVM.sh
+++ ArchVM.sh
@@ -3,5 +3,5 @@
     bash 0-preinstall.sh
     arch-chroot /mnt pacman -S git --needed --noconfirm
     arch-chroot /mnt git clone https://github.com/MonacoMV/install.git
-    arch-chroot /mnt bash /ArchVM-installscript/1-setup.sh
-    arch-chroot /mnt bash /ArchVM-installscript/2-user.sh
+    arch-chroot /mnt bash /install/1-setup.sh
+    arch-chroot /mnt bash /install/2-user.sh
